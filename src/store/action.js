abd360d7aeea6449e3053a9d224682fd04ba09d9
M-Blaze <raimoulik@gmail.com>
2019-12-14 20:35:54 +0545

    Add the note component

--- src/store/action.js
+++ src/store/action.js
@@ -1,10 +1,30 @@
-import { fetchNotebooks as fetchNotebook } from "../api/notebooks";
+import { fetchNotebooks as fetchNotebooksApi } from "../api/notebooks";
+import { fetchNotes as fetchNotesApi } from "../api/notebooks";
+import { fetchActiveNotebook } from "../api/notebooks";
 
 export const fetchNotebooks = () => dispatch => {
-  fetchNotebook().then(notebooks => {
+  fetchNotebooksApi().then(notebooks => {
     return dispatch({
       type: "SET_NOTEBOOKS",
       payload: notebooks
+    });
+  });
+};
+
+export const fetchNotes = notebookId => dispatch => {
+  fetchNotesApi(notebookId).then(notes => {
+    dispatch({
+      type: "SET_NOTES",
+      payload: notes
+    });
+  });
+};
+
+export const setActiveNotebook = notebookId => dispatch => {
+  fetchActiveNotebook(notebookId).then(notebook => {
+    dispatch({
+      type: "SET_ACTIVE_NOTEBOOK",
+      payload: notebook
     });
   });
 };
--- src/views/notebooks/components/notes.jsx
+++ src/views/notebooks/components/notes.jsx
@@ -1,22 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import NotesIcon from "@material-ui/icons/Notes";
-
-const Notes = () => {
-  return (
-    <div className="notebook-content">
-      <div className="block-title">
-        <div className="icon-wrapper">
-          <NotesIcon />
+import { connect } from "react-redux";
+import { fetchNotes } from "../../../store/action";
+class Notes extends Component {
+  componentDidUpdate(prevProps) {
+    if (prevProps.activeNotebook !== this.props.activeNotebook) {
+      this.props.fetchNotes(this.props.activeNotebook.id);
+    }
+  }
+  render() {
+    return (
+      <div className="notebook-content">
+        <div className="block-title">
+          <div className="icon-wrapper">
+            <NotesIcon />
+          </div>
+          <div className="title-text">{this.props.activeNotebook.name}</div>
+          <div className="icon-holder">
+            <MoreHorizIcon />
+          </div>
         </div>
-        <div className="title-text">General</div>
-        <div className="icon-holder">
-          <MoreHorizIcon />
+        <div className="card-block">
+          {this.props.notes.map(note => {
+            return (
+              <div className="card" key={note.id}>
+                <div className="card-title">
+                  <h4>{note.title}</h4>
+                  <div className="icon-holder">
+                    <MoreHorizIcon />
+                  </div>
+                </div>
+                <div className="card-content">
+                  <p>{note.content}</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
-      <div className="card-block"></div>
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    activeNotebook: state.activeNotebook,
+    notes: state.notes
+  };
 };
 
-export default Notes;
+export default connect(mapStateToProps, { fetchNotes })(Notes);
