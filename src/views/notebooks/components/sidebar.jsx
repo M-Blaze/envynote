3f62c8489535270125d957564b07aa59d72d24f2
M-Blaze <raimoulik@gmail.com>
2019-12-14 19:06:59 +0545

    connected  fetchNotebook action with the component

--- src/views/notebooks/components/sidebar.jsx
+++ src/views/notebooks/components/sidebar.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
 import AddIcon from "@material-ui/icons/Add";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import NotesIcon from "@material-ui/icons/Notes";
+import { fetchNotebooks } from "../../../store/action";
 
 class Sidebar extends Component {
+  componentDidMount() {
+    this.props.fetchNotebooks();
+  }
   render() {
     return (
       <div className="notebook-bar">
@@ -11,10 +19,32 @@ class Sidebar extends Component {
             <AddIcon />
           </div>
         </div>
-        <div className="notebooks-list"></div>
+        <div className="notebooks-list">
+          {this.props.notebooks.map(notebook => {
+            return (
+              <Link to={`/notebooks/${notebook.id}`}>
+                <div className="notebook">
+                  <div className="icon-wrap">
+                    <NotesIcon />
+                  </div>
+                  <div className="notebook-title">{notebook.name}</div>
+                  <div className="icon-holder">
+                    <MoreVertIcon />
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
       </div>
     );
   }
 }
 
-export default Sidebar;
+const mapStateToProps = state => {
+  return {
+    notebooks: state.notebooks
+  };
+};
+
+export default connect(mapStateToProps, { fetchNotebooks })(Sidebar);
