cb2c408b749e5b6a38fd017f553485f1ff1c2559
M-Blaze <raimoulik@gmail.com>
2020-02-11 14:31:38 +0545

    add pwa functionality

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import store from "./store";
 import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -11,3 +12,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+serviceWorker.register();
